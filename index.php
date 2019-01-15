7067b73cba9cf0be235a4f166282b9dbea71a262
freddyyeddy <freddyyeddy@gmail.com>
2019-01-15 12:43:40 -0900

    Revert "Un F%%%ing this think"
    This reverts commit 2b023b87a60bf4c78eaad958c4d017d1fd4641fd.

--- index.php
+++ index.php
@@ -19,6 +19,8 @@ require("inc/var.php");
 
 <script>
 
+<<<<<<< HEAD
+=======
 function sidebar(){
 $(".sidebar").toggleClass("open transition");
 }
@@ -61,7 +63,7 @@ console.log(array);
 }
 
 
-
+>>>>>>> master
 // 	function for remaing characters in textarea
 	function remaing_char(box,out,max){
 
@@ -177,13 +179,20 @@ console.log(array);
 
 
 		var oTable;
+<<<<<<< HEAD
 
+  $(document).ready(function() {
+=======
+>>>>>>> master
 
   $(document).ready(function() {
 
+<<<<<<< HEAD
 
-  $(document).ready(function() {
+// 		Functions
 
+
+=======
 // 		Functions
 
 // add("https://ecowalaska.bpweb.bp.com/permitvision/editor/#/certificate/BP05-K-GPB-GC2#298773?selectedTab=actions&wizardStep=","New Test Permit #");
@@ -220,7 +229,7 @@ $('#Examples ul').prepend(
 )
 
 };
-
+>>>>>>> master
 
 // 		Copy Text To Clipboard
 		function copyTextToClipboard(text) {
@@ -371,10 +380,11 @@ var separr = value.split(', ');
 
 		yadcf.init(oTable, [
 
-
+<<<<<<< HEAD
 					}
 				},
-
+=======
+>>>>>>> master
 // 			{column_number : 1},
 // 			{column_number : 2},
 // 			{column_number : 3},
@@ -493,13 +503,19 @@ Poop;
 
 
 </script>
+<<<<<<< HEAD
 
+=======
+>>>>>>> master
 </head>
 <body>
 	  <svg display="none" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="768" height="800" viewBox="0 0 768 800"><defs><g id="icon-close"><path class="path1" d="M31.708 25.708c-0-0-0-0-0-0l-9.708-9.708 9.708-9.708c0-0 0-0 0-0 0.105-0.105 0.18-0.227 0.229-0.357 0.133-0.356 0.057-0.771-0.229-1.057l-4.586-4.586c-0.286-0.286-0.702-0.361-1.057-0.229-0.13 0.048-0.252 0.124-0.357 0.228 0 0-0 0-0 0l-9.708 9.708-9.708-9.708c-0-0-0-0-0-0-0.105-0.104-0.227-0.18-0.357-0.228-0.356-0.133-0.771-0.057-1.057 0.229l-4.586 4.586c-0.286 0.286-0.361 0.702-0.229 1.057 0.049 0.13 0.124 0.252 0.229 0.357 0 0 0 0 0 0l9.708 9.708-9.708 9.708c-0 0-0 0-0 0-0.104 0.105-0.18 0.227-0.229 0.357-0.133 0.355-0.057 0.771 0.229 1.057l4.586 4.586c0.286 0.286 0.702 0.361 1.057 0.229 0.13-0.049 0.252-0.124 0.357-0.229 0-0 0-0 0-0l9.708-9.708 9.708 9.708c0 0 0 0 0 0 0.105 0.105 0.227 0.18 0.357 0.229 0.356 0.133 0.771 0.057 1.057-0.229l4.586-4.586c0.286-0.286 0.362-0.702 0.229-1.057-0.049-0.13-0.124-0.252-0.229-0.357z"></path></g></defs></svg>
 
 
+<<<<<<< HEAD
+=======
 
+>>>>>>> master
 	<div class="wrapper">
 	<div class="grid" style="margin-left: 1em;">
 	<div class="col-1-4">
