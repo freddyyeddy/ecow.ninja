6818092ff6e3d215514cbaf413c1a29c53ec868d
Adam Bordelon <freddyyeddy@gmail.com>
2019-02-10 14:26:59 -0900

    hopefully fixing ajax error

--- inc/add.php
+++ inc/add.php
@@ -1,18 +1,20 @@
 <?php
+    header('Content-type: application/json');
+  
 require('var.php');
 
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
 $query =  "INSERT INTO `hazmit`(`id`, `hazard`, `mitigation`, `source`, `tags`, `crafts`, `review`, `bad`) VALUES ('','$_POST[hazard]', '$_POST[mitigation]', '$_POST[source]', '$_POST[tags]', '$_POST[crafts]', 0, 1)";
-echo $query;
+$response_array['status'] = 'success'; 
 	db_select($query);
 }else{
-	echo "no post data";
+	$response_array['status'] = 'error';  
 		 
 		 }
 
 
-
+  echo json_encode($response_array);
 
 ?>
