abd72f7115773e2a2c777bad8783837a010a83c7
Adam Bordelon <freddyyeddy@gmail.com>
2019-02-12 16:14:49 -0900

    debugging

--- inc/add.php
+++ inc/add.php
@@ -8,7 +8,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
 $query =  "INSERT INTO `hazmit`(`id`, `hazard`, `mitigation`, `source`, `tags`, `crafts`, `review`, `bad`) VALUES ('','$_POST[hazard]', '$_POST[mitigation]', '$_POST[source]', '$_POST[tags]', '$_POST[crafts]', 0, 1)";
 
-	db_query($query);
+	echo db_query($query);
 	$response_array['status'] = 'success'; 
 }else{
 	$response_array['status'] = 'error';  
