9c0339eecc8f7e74e29815660f67f27aba3a7c81
Adam Bordelon <freddyyeddy@gmail.com>
2019-02-12 17:08:29 -0900

    test test

--- inc/add.php
+++ inc/add.php
@@ -2,19 +2,14 @@
 header('Content-type: application/json');
   
 require('var.php');
-
-
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-
-$query =  "INSERT INTO `hazmit`(`id`, `hazard`, `mitigation`, `source`, `tags`, `crafts`, `review`, `bad`) VALUES ('','addslashes($_POST[hazard])', 'addslashes($_POST[mitigation])', '$_POST[source]', '$_POST[tags]', '$_POST[crafts]', 0, 1)";
-
+$query =  "INSERT INTO `hazmit`(`id`, `hazard`, `mitigation`, `source`, `tags`, `crafts`, `review`, `bad`) VALUES ('',\'addslashes($_POST[hazard])\', \'addslashes($_POST[mitigation])\', \'$_POST[source]\', \'$_POST[tags]\', \'$_POST[crafts]\', 0, 1)";
 	db_query($query);
 	$response_array['status'] = 'success'; 
 }else{
 	$response_array['status'] = 'error';  
 		 
 		 }
-
   echo json_encode($response_array);
  echo mysqli_error(db_connect());
 //  echo "test";
