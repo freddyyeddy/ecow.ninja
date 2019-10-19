174b8eefc35009ac10ea18235fc6589208374b6a
Adam Bordelon <freddyyeddy@gmail.com>
2019-10-18 20:55:14 -0800

    fixing email link

--- AA/inc/Main.php
+++ AA/inc/Main.php
@@ -94,7 +94,7 @@ $mail->Body    = <<<EmailHTML
                                 <table border="0" cellpadding="0" cellspacing="0" style="border-collapse: separate; mso-table-lspace: 0pt; mso-table-rspace: 0pt; width: auto;">
                                   <tbody>
                                     <tr>
-                                      <td style="font-family: sans-serif; font-size: 14px; vertical-align: top; background-color: #3498db; border-radius: 5px; text-align: center;"> <a href="//localhost/ecow_ninja/inc/tst2.php?mg=$magic&f=$fac" target="_blank" style="display: inline-block; color: #ffffff; background-color: #3498db; border: solid 1px #3498db; border-radius: 5px; box-sizing: border-box; cursor: pointer; text-decoration: none; font-size: 14px; font-weight: bold; margin: 0; padding: 12px 25px; text-transform: capitalize; border-color: #3498db;">Magic Link</a> </td>
+                                      <td style="font-family: sans-serif; font-size: 14px; vertical-align: top; background-color: #3498db; border-radius: 5px; text-align: center;"> <a href="https://ecow.ninja/AA/AA.php?mg=$magic&f=$fac" target="_blank" style="display: inline-block; color: #ffffff; background-color: #3498db; border: solid 1px #3498db; border-radius: 5px; box-sizing: border-box; cursor: pointer; text-decoration: none; font-size: 14px; font-weight: bold; margin: 0; padding: 12px 25px; text-transform: capitalize; border-color: #3498db;">Magic Link</a> </td>
                                     </tr>
                                   </tbody>
                                 </table>
