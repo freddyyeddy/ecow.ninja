8fabd2445cb6974b32519d2b0a85f372ef0685a1
Adam Bordelon <freddyyeddy@gmail.com>
2019-11-16 11:13:54 -0900

    Update pushtest.php
    adding test curl calls

--- inc/pushtest.php
+++ inc/pushtest.php
@@ -1,4 +1,11 @@
 <?
-phpinfo();
+require 'PushBullet.class.php';
+try {
+$p = new PushBullet('o.kHJHbBgZ9INKRwb38Z5vT1nx5BX3kozN');
+$p->pushNote(NULL, 'Test Push Title', 'Someone Opend the Page');
 
+} catch (PushBulletException $e) {
+  // Exception handling
+  die($e->getMessage());
+}
 ?>
