15486fb6133e88bd9e79e9dbf23cd6ac172371ec
Adam Bordelon <freddyyeddy@gmail.com>
2019-11-15 18:47:04 -0900

    fixing upper case for tags and crafts
    experimental may bug

--- inc/get.php
+++ inc/get.php
@@ -9,6 +9,13 @@ if($rows === false) {
     // Handle error - inform administrator, log to file, show error page, etc.
 }
 
+array_walk_recursive($rows, function (&$v, $k) { 
+    if($k == 'tags' || $k == 'crafts'){ 
+        $v = ucwords(strtolower($v), ","); 
+    } 
+});
+
+
 echo json_encode($rows);
 
 ?>
