00d38c4272a270cdfa09da67689c27bcc7e969e3
Adam Bordelon <freddyyeddy@gmail.com>
2019-11-23 07:22:47 -0900

    cleaning up HTML markup
    this should help with IExplorer Support

--- index.php
+++ index.php
@@ -582,6 +582,10 @@ Poop;
 
 
 </script>
+			<style>
+		  [id^=yadcf-filter-wrapper--]{ width: 25vw;
+			}
+		</style>
 </head>
 <body>
 	  <svg display="none" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="768" height="800" viewBox="0 0 768 800"><defs><g id="icon-close"><path class="path1" d="M31.708 25.708c-0-0-0-0-0-0l-9.708-9.708 9.708-9.708c0-0 0-0 0-0 0.105-0.105 0.18-0.227 0.229-0.357 0.133-0.356 0.057-0.771-0.229-1.057l-4.586-4.586c-0.286-0.286-0.702-0.361-1.057-0.229-0.13 0.048-0.252 0.124-0.357 0.228 0 0-0 0-0 0l-9.708 9.708-9.708-9.708c-0-0-0-0-0-0-0.105-0.104-0.227-0.18-0.357-0.228-0.356-0.133-0.771-0.057-1.057 0.229l-4.586 4.586c-0.286 0.286-0.361 0.702-0.229 1.057 0.049 0.13 0.124 0.252 0.229 0.357 0 0 0 0 0 0l9.708 9.708-9.708 9.708c-0 0-0 0-0 0-0.104 0.105-0.18 0.227-0.229 0.357-0.133 0.355-0.057 0.771 0.229 1.057l4.586 4.586c0.286 0.286 0.702 0.361 1.057 0.229 0.13-0.049 0.252-0.124 0.357-0.229 0-0 0-0 0-0l9.708-9.708 9.708 9.708c0 0 0 0 0 0 0.105 0.105 0.227 0.18 0.357 0.229 0.356 0.133 0.771 0.057 1.057-0.229l4.586-4.586c0.286-0.286 0.362-0.702 0.229-1.057-0.049-0.13-0.124-0.252-0.229-0.357z"></path></g></defs></svg>
@@ -590,16 +594,12 @@ Poop;
 
 	<div class="wrapper">
 	<div>
-		<style>
-		  [id^=yadcf-filter-wrapper--]{ width: 25vw;
-			}
-		</style>
 		<table>
 			<tr>
 	<td style="border-left: none;" >
 		<button class="showex" onclick="sidebar(this)">Favorites</button>
 		<nav class="sidebar transition">
-				<span id=Examples>
+				<div id=Examples>
 					<ul>
 					<li>
 						<form id="add" onkeypress="return event.keyCode != 13;">
@@ -611,7 +611,7 @@ Poop;
 						</form>
 					</li>
 					</ul>
-				</span>
+				</div>
 			</nav>
 		</td>
 	<td  style="width: 20%; border-left: none;" id="facflag"></td>
@@ -674,6 +674,8 @@ Table;
 				<th>bad</th>
 			</tr>
 		</thead>
+				<tbody>
+		</tbody>
 		 <tfoot>
             <tr>
 				<th>Source</th>
@@ -687,8 +689,6 @@ Table;
 				<th>bad</th>
             </tr>
         </tfoot>
-		<tbody>
-		</tbody>
 	</table>
 </div>
 	<?php
@@ -790,7 +790,7 @@ Modalpoo;
 
 					</form>
         </div>
-				<div align="center">
+				<div style="text-align: center;">
 <!-- 					onclick='submitF(event);' -->
 				<input class='button myButton' type='submit' value='Submit' onclick='submitG(event);' id='submithaz'  name='submit'>
 				</div>
