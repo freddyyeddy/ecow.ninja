bef41e7ab30077dbebdf68526a99d4f17c0b4432
Adam Bordelon <freddyyeddy@gmail.com>
2020-04-03 03:09:32 -0800

    test

--- inc/testing.php
+++ inc/testing.php
@@ -3,9 +3,10 @@
 $privKey = file_get_contents(dirname(__FILE__)."/caltrops.tabi");
 $encrypted = file_get_contents(dirname(__FILE__)."/ninja.kunai");
 //decrypting variable file with private key and Adding Variables
-//openssl_private_decrypt($encrypted, $decrypted, $privKey);
+openssl_private_decrypt($encrypted, $decrypted, $privKey);
+echo $decrypted;
 $ninjasecrets = "";
-$Split = str_split($encrypted , 685);
+$Split = str_split($encrypted , 684);
 ForEach($Split as $Part)
 {
   openssl_private_decrypt(base64_decode($Part), $PartialData, $privKey);
