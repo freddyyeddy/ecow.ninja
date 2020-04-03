411b2501ccc05e5c86ea5c35462a67b6c225f9e2
Adam Bordelon <freddyyeddy@gmail.com>
2020-04-03 14:00:14 -0800

    Commenting out kunai test.

--- inc/testing.php
+++ inc/testing.php
@@ -1,16 +1,16 @@
 <?php
-//Reading Super Secret Variables
-$privKey = file_get_contents(dirname(__FILE__)."/caltrops.tabi");
-$encrypted = file_get_contents(dirname(__FILE__)."/ninja.kunai");
-//decrypting variable file with private key and Adding Variables
-// openssl_private_decrypt($encrypted, $decrypted, $privKey);
-// echo $decrypted;
-$ninjasecrets = "";
-$Split = str_split($encrypted , 684);
-ForEach($Split as $Part)
-{
-  openssl_private_decrypt(base64_decode($Part), $PartialData, $privKey);
-  $ninjasecrets.= $PartialData;
-}
-eval($ninjasecrets);
+// //Reading Super Secret Variables
+// $privKey = file_get_contents(dirname(__FILE__)."/caltrops.tabi");
+// $encrypted = file_get_contents(dirname(__FILE__)."/ninja.kunai");
+// //decrypting variable file with private key and Adding Variables
+// // openssl_private_decrypt($encrypted, $decrypted, $privKey);
+// // echo $decrypted;
+// $ninjasecrets = "";
+// $Split = str_split($encrypted , 684);
+// ForEach($Split as $Part)
+// {
+//   openssl_private_decrypt(base64_decode($Part), $PartialData, $privKey);
+//   $ninjasecrets.= $PartialData;
+// }
+// eval($ninjasecrets);
 ?>
